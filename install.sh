2ccf9aeabc8a5455a57191d36182dc6f8d692ae7
PryosCode <info@pryoscode.net>
2021-04-16 20:08:32 +0200

    Add logo

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-DIR=`realpath .`
+DIR=`realpath $(dirname $0)`
 
 function link {
 	mkdir -p `dirname $2`
 	ln -s $DIR/$1 $2
 }
+
+echo
+cat $DIR/img/logo.txt
+echo
+echo https://github.com/PryosCode/dotfiles
+echo
 
 link home/config/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
 link home/config/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
